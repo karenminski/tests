88610d3d57aa3c58a2d36d49eb1f0e8534724ad0
Karen Minski <43170840+karenminski@users.noreply.github.com>
2019-11-04 12:39:52 +0200

    Ül2

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import {TiHeartFullOutline} from "react-icons/ti";
 
-const Test2 = () => {
+class Test2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    };
+  }
+  onClick() {
+    this.setState(oldState => ({
+      likes: oldState.likes + 1
+    }));
+  }
+
+render() {
   return (
     <div>
       <div className={"description"}>
@@ -13,13 +26,14 @@ const Test2 = () => {
 
       <div style={{display:"flex", alignItems: "center"}}>
         <div>
-          Likes: [replace me]
+          Likes: {this.state.likes}
         </div>
-        <TiHeartFullOutline />
+        <TiHeartFullOutline onClick={this.onClick.bind(this)} />
         <div>Click me</div>
       </div>
     </div>
   );
-};
+}
+}
 
 export default Test2;
