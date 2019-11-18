59741c1bec35584d28fed9ca9c84e08cef3abbfc
Karen <43170840+karenminski@users.noreply.github.com>
2019-11-18 13:27:27 +0200

    fix test5

--- server/orders.router.js
+++ server/orders.router.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const {Order} = require("./order.model.js");
 const mongoose = require("mongoose");
 
-/*router.get("/", async (req, res)=>{
+router.get("/", async (req, res)=>{
   const xs = await Order.find({});
   res.send(xs);
-});*/
+});
 
-router.post("/addname", (req, res) => {
-  var myData = new Order(req.body);
-  myData.save()
-    .then(item => {
-      res.send("Order saved to database");
-    })
-    .catch(err => {
-      res.status(400).send("unable to save order to database");
-    });
+router.post("/", (req, res) => {
+  const order = new Order(req.body);
+  order.save((err) => {
+    if(err) {
+      console.log(err);
+      res.send(500);
+    }
+    res.send(200);
+  })
 });
 
 module.exports = router;
