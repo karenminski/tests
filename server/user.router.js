dd840501d8aecb852a18f960a543828be018962e
Karen <43170840+karenminski@users.noreply.github.com>
2019-12-09 09:19:00 +0200

    test9

--- server/user.router.js
+++ server/user.router.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const {User} = require("./user.model.js");
+const {
+  User
+} = require("./user.model.js");
 const mongoose = require("mongoose");
 const multer = require('multer');
 
-router.get("/", async (req, res)=>{
+router.get("/", async (req, res) => {
   const xs = await User.find({});
   res.send(xs);
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  const filter = {
+    personalCode: req.body.personalCode
+  };
+  const doc = req.body;
+  if (req.body.fullName == "") return res.send(500);
+  if (req.body.address == "") return res.send(500);
+  if (req.body.phoneNumber == "") return res.send(500);
+  if (req.body.personalCode == "") return res.send(500);
+  const options = {
+    upsert: true
+  };
+  try {
+    const {
+      n,
+      nModified
+    } = await User.updateOne(filter, doc, options);
+    console.log("n", n, "nModified", nModified);
+    res.send(200);
+  } catch {
+    res.send(500);
+  }
+});
+
+/*router.post("/", (req, res) => {
   const user = new User(req.body);
   user.save((err) => {
     if(err) {
@@ -18,6 +44,6 @@ router.post("/", (req, res) => {
     }
     res.send(200);
   });
-});
+});*/
 
 module.exports = router;
